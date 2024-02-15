a073c4bb9ca994a84270efa8fa8e045ee2f026f5
Coder <Coder@DESKTOP-JT59BER>
2024-02-16 00:57:03 +0300

    DRY

--- Entities/Abstract/IEntity.cs
+++ Entities/Abstract/IEntity.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Entities.Entity
+namespace Entities.Abstract
 {
-    public class Instructor
+    public interface IEntity
     {
     }
 }
