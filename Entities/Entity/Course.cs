4638f4a8641a226424ddf0d4346895cbe3b1222a
Coder <Coder@DESKTOP-JT59BER>
2024-02-15 20:13:53 +0300

    Add project files.

---
+++ Entities/Entity/Course.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Entity
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+        public string? CourseName { get; set; }
+        public string? CourseDecription { get; set; }
+        public string? CourseImage{ get; set; }
+        public string? InstructorImage{ get; set; }
+
+
+
+
+    }
+}
